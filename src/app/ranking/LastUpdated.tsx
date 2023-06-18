8404e8dacfcaee288089b7217c525bcdd2930261
Ben Unyolo <44584958+BenUnyolo@users.noreply.github.com>
2023-06-18 18:46:24 -0400

    adds console logs

--- src/app/ranking/LastUpdated.tsx
+++ src/app/ranking/LastUpdated.tsx
@@ -9,5 +9,8 @@ interface ILastUpdatedProps {
 export const LastUpdated: React.FC<ILastUpdatedProps> = ({ updatedDate }) => {
   const currentDate = new Date();
 
+  console.log("currentDate: ", currentDate);
+  console.log("updatedDate: ", updatedDate);
+
   return <>Last updated {formatRelative(updatedDate, currentDate)}</>;
 };
